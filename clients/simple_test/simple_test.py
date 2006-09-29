12eeca04732ad8732275bdcab531ba22d495c3ad
Ondřej Surý <ondrej.sury@nic.cz>
2006-09-29 21:26:36 +0000

    simple_test: jednoduchy test, tak aby se dala CORBA automatizovane testovat
    git-svn-id: file:///data/svn/enum/pyccReg/trunk@1464 b60c293d-5e0d-0410-87b2-fb9704ccd4b5

--- clients/simple_test/simple_test.py
+++ clients/simple_test/simple_test.py
@@ -1,46 +1,32 @@
 #!/usr/bin/env python
 
-import socket
-from omniORB import CORBA
+from omniORB import CORBA, importIDL
+from ConfigParser import ConfigParser
+import sys
 import CosNaming, ccReg
+from pprint import pprint
+import corbaparser
 
-MAX_MSG_LEN = 1024
+conf = ConfigParser()
+conf.read("./simple_test.conf")
 
+idl = conf.get('corba', 'idl')
+ior = conf.get('corba', 'ior')
 
-# Initialise the ORB
-orb = CORBA.ORB_init(["-ORBInitRef", "NameService=corbaname::localhost"],
-		     CORBA.ORB_ID)
-# Obtain a reference to the root naming context
-obj = orb.resolve_initial_references("NameService")
-rootContext = obj._narrow(CosNaming.NamingContext)
-if rootContext is None:
-	raise ZoneException("Failed to narrow the root naming context")
-# Resolve the name "ccReg.context/ZoneGenerator.Object"
-name = [CosNaming.NameComponent("ccReg", "context"),
-	CosNaming.NameComponent("PyWhois", "Object")]
-obj = rootContext.resolve(name)
-# Narrow the object to an ccReg::Whois
-whois_obj = obj._narrow(ccReg.Whois)
-if (whois_obj is None):
-	raise ZoneException("Object reference is not an ccReg::Whois")
-#
-#
-(domain, timestamp) = whois_obj.getDomain("1.1.1.5.4.7.2.2.2.e164.arpa")
-print domain.fqdn
+ccReg = sys.modules[importIDL(idl)[0]]
 
-#(timestamp, domain.fqdn, domain.created, domain.expired,
-#		domain.registrarName, domain.registrarUrl)
-#     		for tech in domain.tech:
-#     		for ns in domain.ns:
-#
-#except ccReg.Whois.DomainError, e:
-#	if e.type == ccReg.WE_NOTFOUND:
-#		print "NOT FOUND"
-#	if e.type in (ccReg.WE_INVALID, ccReg.WE_DOMAIN_LONG):
-#		print "INVALID"
-#	if e.type == ccReg.WE_DOMAIN_BAD_ZONE:
-#		print "BAD ZONE"
-#except ccReg.Whois.WhoisError, e:
-#	print "WHOIS ERROR"
-#except Exception, e:
-#	print e
+orb = CORBA.ORB_init(["-ORBInitRef", "NameService=%s" % ior], CORBA.ORB_ID)
+
+rootContext = orb.string_to_object(ior)._narrow(CosNaming.NamingContext)
+
+epp = rootContext.resolve([CosNaming.NameComponent("ccReg", "context"), CosNaming.NameComponent('EPP', "Object")])._narrow(ccReg.EPP)
+
+parser = corbaparser.CorbaParser('utf-8')
+
+handle = 'CID:SURY-CZ.NIC'
+
+(response,value) = parser.parse(epp.ContactInfo(handle, 0, "dummy-0", ''))
+
+pprint(response['errCode'])
+
+sys.exit(0)
