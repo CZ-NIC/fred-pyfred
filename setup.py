21344f629c5b84c84ce96e1175814aec86858334
Aleš Doležal <ales.dolezal@nic.cz>
2008-04-23 10:18:31 +0000

    Another simplification in setup.py:  -removed useles Build_ext class (no need to build C extensions).  -Build_scripts class considerably simplified.
    git-svn-id: file:///data/svn/enum/pyfred/trunk@5218 b60c293d-5e0d-0410-87b2-fb9704ccd4b5

--- setup.py
+++ setup.py
@@ -271,103 +271,13 @@ class Build_py (build_py, object):
 #class Build_py
 
 class Build_scripts(build_scripts):
-    def copy_scripts (self):
-        """Copy each script listed in 'self.scripts'; if it's marked as a
-        Python script in the Unix way (first line matches 'first_line_re',
-        ie. starts with "\#!" and contains "python"), then adjust the first
-        line to refer to the current Python interpreter as we copy.
-        """
-        #DIST line added
-        self.srcdir = g_srcdir
-        self.mkpath(self.build_dir)
-        outfiles = []
-        for script in self.scripts:
-            adjust = 0
-            #DIST line added
-            script = os.path.join(self.srcdir, script)
-            script = util.convert_path(script)
-            outfile = os.path.join(self.build_dir, os.path.basename(script))
-            outfiles.append(outfile)
-
-            if not self.force and not dep_util.newer(script, outfile):
-                log.debug("not copying %s (up-to-date)", script)
-                continue
-
-            # Always open the file, but ignore failures in dry-run mode --
-            # that way, we'll get accurate feedback if we can read the
-            # script.
-            try:
-                f = open(script, "r")
-            except IOError:
-                if not self.dry_run:
-                    raise
-                f = None
-            else:
-                first_line = f.readline()
-                if not first_line:
-                    self.warn("%s is an empty file (skipping)" % script)
-                    continue
-
-                match = first_line_re.match(first_line)
-                if match:
-                    adjust = 1
-                    post_interp = match.group(1) or ''
-
-            if adjust:
-                log.info("copying and adjusting %s -> %s", script,
-                         self.build_dir)
-                if not self.dry_run:
-                    outf = open(outfile, "w")
-                    if not sysconfig.python_build:
-                        outf.write("#!%s%s\n" %
-                                   (self.executable,
-                                    post_interp))
-                    else:
-                        outf.write("#!%s%s\n" %
-                                   (os.path.join(
-                            sysconfig.get_config_var("BINDIR"),
-                            "python" + sysconfig.get_config_var("EXE")),
-                                    post_interp))
-                    outf.writelines(f.readlines())
-                    outf.close()
-                if f:
-                    f.close()
-            else:
-                f.close()
-                self.copy_file(script, outfile)
-
-        if os.name == 'posix':
-            for file in outfiles:
-                if self.dry_run:
-                    log.info("changing mode of %s", file)
-                else:
-                    oldmode = os.stat(file)[stat.ST_MODE] & 07777
-                    newmode = (oldmode | 0555) & 07777
-                    if newmode != oldmode:
-                        log.info("changing mode of %s from %o to %o",
-                                 file, oldmode, newmode)
-                        os.chmod(file, newmode)
-    # copy_scripts ()
+    def run(self):
+        #DIST next 4 lines added
+        self.scrdir = g_srcdir
+        for i in range(len(self.scripts)):
+            self.scripts[i] = os.path.join(self.scrdir, self.scripts[i])
+        build_scripts.run(self)
 #class Build_scripts
-
-class Build_ext(build_ext):
-    def build_extension(self, ext):
-        #DIST line added
-        self.srcdir = g_srcdir
-        sources = ext.sources
-        if sources is None or type(sources) not in (types.ListType, types.TupleType):
-            raise DistutilsSetupError, \
-                    ("in 'ext_modules' option (extension '%s'), " +
-                            "'sources' must be present and must be " +
-                            "a list of source filenames") % ext.name
-        new_sources = []
-        for source in sources: 
-            #DIST line changed
-            new_sources.append(os.path.join(self.srcdir,source))
-        ext.sources = new_sources
-        return build_ext.build_extension(self, ext)
-    #build_extension()
-#class Build_ext
 
 class Install (install.install, object):
     user_options = []
@@ -1393,7 +1303,6 @@ def main():
                 cmdclass = { "config":Config,
                              "clean":Clean,
                              "build_py":Build_py,
-                             "build_ext":Build_ext,
                              "build_scripts":Build_scripts,
                              "install":Install,
                              "install_data":Install_data,
@@ -1435,7 +1344,7 @@ def main():
                     ),
                     (ETC_FRED_DIR, [
                         os.path.join("build", "pyfred.conf"),
-                        os.path.join("build", "genzone.conf"]),
+                        os.path.join("build", "genzone.conf")]),
                     ]
                 )
         return True
