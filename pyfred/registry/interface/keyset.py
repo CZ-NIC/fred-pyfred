52660b10df265e2ca53cdb7a84791be53fa94c2c
Zdeněk Böhm <zdenek.bohm@nic.cz>
2012-11-19 16:56:05 +0100

    Ticket #7685 - Add dummy DomainBrowser.KeysetDetail.

--- pyfred/registry/interface/keyset.py
+++ pyfred/registry/interface/keyset.py
@@ -40,7 +40,54 @@ class KeysetInterface(ListMetaInterface):
             sequence<DNSKey> dnskeys;
             ObjectStatusSeq  status_list;
         };
+
+        struct DSRecord
+        {
+            long key_tag;
+            long alg;
+            long digest_type;
+            string digest;
+            long max_sig_life;
+        };
+
+        struct DNSKey
+        {
+            unsigned short flags;
+            unsigned short protocol;
+            unsigned short alg;
+            string         key;
+        };
         """
         self.logger.log(self.logger.DEBUG, 'Call KeysetInterface.getKeysetDetail(keyset="%s", handle="%s")' % (keyset, handle))
         handle = normalize_and_check_handle(self.logger, handle) # Registry.DomainBrowser.INCORRECT_USAGE
-        return Registry.DomainBrowser.KeysetDetail(id=0)
+
+        key = "BQEAAAABt3LenoCVTV0okqKYPDnnVJqvwCD9MKJNXg8fcOCdLQYncyoehpwM5RK2UkZDcDxWkMo7yMa35ej+Mhpa" \
+              "ji9si4xXD+Syl4Q06LFiFkdN/5GlVlrIdE3GW7zC7Z4sS14Vz8FbYfcRmhsh19Ob718jGZneGfw2UPbvkyxUR8wD" \
+              "7mguZn02fQ6tjj/Ktp4uSW9tpz3bjGMo2rX+iZk4xgbPaesAOlR/AaHdatGZsWC9CPon8mnLZeu6czm8CBDgBmnf" \
+              "3PE8c5+uyWj1Pw4pp0VQmnX5UrnuGpErg7qXhJm7wY2CRVRMcLX3zmjVWXW1uT9JFh2G+/pZzxnASfKKltZpuw=="
+
+        PUBLIC_DATA, PRIVATE_DATA = range(2)
+        return (Registry.DomainBrowser.KeysetDetail(
+                id=130,
+                handle=handle,
+                roid='C0000000003-CZ',
+                registrar='REG-DESIGNATED',
+                create_date='2012-03-14 11:16:28.516926',
+                transfer_date='',
+                update_date='',
+                create_registrar='REG-CREATED',
+                update_registrar='',
+                auth_info='password',
+                admins=('CONTACT04',),
+                dsrecords = (
+                    Registry.DomainBrowser.DSRecord(27979, 5, 1, 'FF11E740A0254EC63C738A47E52ABF3AD91D8C43', 0),
+                    Registry.DomainBrowser.DSRecord(27980, 5, 1, 'AA11E740A0254EC63C738A47E52ABF3AD91D8C00', 0),
+                    ),
+                dnskeys = (
+                    Registry.DomainBrowser.DNSKey(257, 3, 5, key),
+                    Registry.DomainBrowser.DNSKey(256, 4, 6, key),
+                ),
+
+                status_list = (),
+                ),
+                Registry.DomainBrowser.DataAccessLevel._item(PRIVATE_DATA))
