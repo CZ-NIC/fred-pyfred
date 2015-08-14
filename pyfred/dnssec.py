c426bf48dcc3790922dc56a2b5ed2acfc7002591
Jiri Sadek <jiri.sadek@nic.cz>
2015-08-14 11:14:26 +0200

    Ticket #13777 - add configuration for sha1 and sha256

--- pyfred/dnssec.py
+++ pyfred/dnssec.py
@@ -1,6 +1,8 @@
 """
 This module gathers various utility functions used in other pyfred's modules.
 """
+import hashlib
+
 
 def countKeyTag(flags, protocol, alg, key):
     """
@@ -21,12 +23,24 @@ def countKeyTag(flags, protocol, alg, key):
         sum += (sum >> 16) & 0xFFFF
         return sum & 0xFFFF
 
-def countDSRecordDigest(fqdn, flags, protocol, alg, key):
+
+
+dsrecord_algorithms = {
+    "sha1": {"func": hashlib.sha1, "type": 1},
+    "sha256": {"func": hashlib.sha256, "type": 2}
+}
+
+
+def count_dsrecord_digest(fqdn, flags, protocol, alg, key, digest_algo):
     """
-    Count SHA256 digest from fqdn and RRDATA of DNSKEY RR according to RFC 4034 (4509)
+    Count digest from fqdn and RRDATA of DNSKEY RR according to RFC 4034 (4509)
+    using specified algorithm
+
+    Returns tuple of digest type and digest
     """
-    import hashlib
-    hash = hashlib.sha256()
+    algo = dsrecord_algorithms[digest_algo]
+
+    hash = algo["func"]()
     labels = fqdn.split(".")
     buffer = ""
     for l in labels:
@@ -34,4 +48,4 @@ def countDSRecordDigest(fqdn, flags, protocol, alg, key):
     buffer += chr(0) + chr(flags >> 8) + chr(flags & 255) + chr(protocol)
     buffer += chr(alg) + key
     hash.update(buffer)
-    return hash.hexdigest().upper()
+    return (algo["type"], hash.hexdigest().upper())
