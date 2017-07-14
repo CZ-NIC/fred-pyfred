adb56a8d4cf8b871ddc9e5139b4f89723e24eadc
Michal Strnad <michal.strnad@nic.cz>
2017-07-14 09:24:56 +0200

    Ticket #19117 - Mailer can collect and return history of calls.

--- pyfred/modules/mailer.py
+++ pyfred/modules/mailer.py
@@ -23,6 +23,8 @@ from email.MIMEMultipart import MIMEMultipart
 from email.MIMEBase import MIMEBase
 from email.MIMEText import MIMEText
 from email.Utils import formatdate, parseaddr
+#transfer collected data
+import pickle
 # IMAP stuff
 import imaplib
 try:
@@ -227,6 +229,7 @@ class Mailer_i (ccReg__POA.Mailer):
         # default configuration
         self.testmode = False
         self.tester = ""
+        self.maxstoredcalls = 0
         self.sendmail = "/usr/sbin/sendmail"
         self.openssl = "/usr/bin/openssl"
         self.fm_context = "fred"
@@ -262,6 +265,14 @@ class Mailer_i (ccReg__POA.Mailer):
                 if tester:
                     self.l.log(self.l.DEBUG, "Tester's address is %s." % tester)
                     self.tester = tester
+            except ConfigParser.NoOptionError, e:
+                pass
+            # maxstoredcalls
+            try:
+                self.maxstoredcalls = conf.getint("Mailer", "maxstoredcalls")
+                if 0 < self.maxstoredcalls:
+                    self.storedcall = []
+                    self.l.log(self.l.DEBUG, "Calls are temporarily stored.")
             except ConfigParser.NoOptionError, e:
                 pass
             # sendmail path
@@ -1095,6 +1106,25 @@ class Mailer_i (ccReg__POA.Mailer):
         and generates an email. The text of the email and operation status must
         be archived in database.
         """
+        try:
+            if 0 < self.maxstoredcalls:
+                if mailtype == ':kujme pickle:':
+                    result = (0, pickle.dumps(self.storedcall))
+                    self.storedcall = []
+                    return result
+                if len(self.storedcall) < self.maxstoredcalls:
+                    self.storedcall.append({
+                        'method':'mailNotify',
+                        'arguments':{
+                            'mailtype':mailtype,
+                            'header':header,
+                            'data':data,
+                            'handles':handles,
+                            'attachs':attachs,
+                            'preview':preview
+                            }})
+        except:
+            pass
         conn = None
         try:
             id = random.randint(1, 9999)
@@ -1148,6 +1178,14 @@ class Mailer_i (ccReg__POA.Mailer):
         Resend email from mail archive with given id. This includes zeroing of
         counter of unsuccessfull sendmail attempts and setting status to 1.
         """
+        try:
+            if 0 < self.maxstoredcalls:
+                if len(self.storedcall) < self.maxstoredcalls:
+                    self.storedcall.append({
+                        'method':'resend',
+                        'arguments':{'mailid':mailid}})
+        except:
+            pass
         try:
             id = random.randint(1, 9999)
             self.l.log(self.l.INFO, "<%d> resend request for email with id = "
